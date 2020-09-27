cd94aeb33d828e7c4d438421f642e653003bc039
berat-ademi <berat.ademi@gmail.com>
2020-09-27 16:54:41 +0200

    second commit

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Berat Ademi',
+		username: 'berat',
+		email: 'bera@live.com'
+	},
+	{
+		id: 2,
+		name: 'Skender Ademi',
+		username: 'skenda',
+		email: 'skenda@live.com'
+	},
+	{
+		id: 3,
+		name: 'Lim Ademi',
+		username: 'lim',
+		email: 'bera@live.com'
+	},
+	{
+		id: 4,
+		name: 'Petrit Ademi',
+		username: 'petrit',
+		email: 'petrit@live.com'
+	},
+	{
+		id: 5,
+		name: 'Endrit Ademi',
+		username: 'endrit',
+		email: 'bera@live.com'
+	},
+	{
+		id: 6,
+		name: 'Driton Ademi',
+		username: 'driton',
+		email: 'driton@live.com'
+	},
+	{
+		id: 7,
+		name: 'Leonor Ademi',
+		username: 'leonor',
+		email: 'leonor@live.com'
+	},
+	{
+		id: 8,
+		name: 'Agon Ademi',
+		username: 'agon',
+		email: 'agon@live.com'
+	},							
+]
